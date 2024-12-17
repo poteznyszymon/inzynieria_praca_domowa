7a6542dafcd0c278625bfa34bd0619b46abc7d49
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-12-17 14:28:57 +0100

    Dodano funckej do liczenia silni

--- projekt.cpp
+++ projekt.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int silnia(int n) {
+    if (n <= 1) return 1;
+    return n * silnia(n - 1);
+}
+
+
 int main() {
     cout << "Projekt 1 - Menu Główne" << endl;
     return 0;
